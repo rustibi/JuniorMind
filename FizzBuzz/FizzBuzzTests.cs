caadd1fce81bd02e1d1506053a5a3c4995f2932d
rustibi <rustibi@gmail.com>
2017-02-16 20:06:03 +0200

    Calculate if is divisible by five

--- FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests.cs
@@ -12,13 +12,30 @@ namespace FizzBuzz
             Assert.AreEqual("FizzBuzz", CalculateFizzBuzz(15));
         }
 
+        [TestMethod]
+        public void DivisibleByFive()
+        {
+            Assert.AreEqual("Buzz", CalculateFizzBuzz(5));
+        }
+
 
         string CalculateFizzBuzz(int number)
         {
             string result = "";
             if (number % 15 == 0)
+            {
                 result = "FizzBuzz";
                 return result;
+            }
+            else if (number % 5 == 0)
+            {
+                result = "Buzz";
+                return result;
+            }
+
+            else
+                return null;
+            
             
         }
     }
