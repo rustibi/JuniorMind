0e305ffa7d342d31b08d84cba2151855797d109c
rustibi <rustibi@gmail.com>
2017-02-18 14:20:51 +0200

    Extract IsMediumDistance function

--- TaxiFare/TaxiFareTests.cs
+++ TaxiFare/TaxiFareTests.cs
@@ -20,8 +20,14 @@ namespace TaxiFare
 
         decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
-            decimal pricePerKm = distanceInKm > 20? 8: 5;
+            decimal pricePerKm = IsMediumDistance(distanceInKm) ? 8: 5;
             return distanceInKm * pricePerKm;
         }
+
+        private static bool IsMediumDistance(int distanceInKm)
+        {
+            return distanceInKm > 20;
+        }
+            
     }
 }
