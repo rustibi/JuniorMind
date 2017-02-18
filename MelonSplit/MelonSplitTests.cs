e8cae06f391e132d5ba4ee6188e66cc834d731d7
rustibi <rustibi@gmail.com>
2017-02-18 18:04:21 +0200

    Calculate melon split if weight is odd

--- MelonSplit/MelonSplitTests.cs
+++ MelonSplit/MelonSplitTests.cs
@@ -12,9 +12,21 @@ namespace MelonSplit
             Assert.AreEqual("Da", CalculateMelonSplit(2));
         }
 
+        [TestMethod]
+        public void WeightMelonOdd()
+        {
+            Assert.AreEqual("Nu", CalculateMelonSplit(3));
+        }
+
+
         string CalculateMelonSplit(int weight)
         {
-            return "Da";
+            if (weight%2 == 0)
+            {
+                return "Da";
+            }
+                
+            return "Nu";
         }
 
     }
