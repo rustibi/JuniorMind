2f510084ad61a61fbe281097073f8f70291a89eb
rustibi <rustibi@gmail.com>
2017-02-18 13:43:48 +0200

    Calculate Day Time Taxi Fare For Short Distances

--- TaxiFare/TaxiFareTests.cs
+++ TaxiFare/TaxiFareTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TaxiFare
 {
     [TestClass]
-    public class UnitTest1
+    public class TaxiFareTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DayTimeFareForShortDistances()
         {
+            Assert.AreEqual(5, CalculateTaxiFare(1, 8));
+        }
+
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
+        {
+            return distanceInKm * 5;
         }
     }
 }
