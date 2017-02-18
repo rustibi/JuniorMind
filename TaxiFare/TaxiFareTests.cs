76fc98f086ee00ea5b462a2b531f8f9a9e0c348f
rustibi <rustibi@gmail.com>
2017-02-18 13:55:23 +0200

    Calculate Day Time Fare For Medium Distances

--- TaxiFare/TaxiFareTests.cs
+++ TaxiFare/TaxiFareTests.cs
@@ -12,9 +12,15 @@ namespace TaxiFare
             Assert.AreEqual(5, CalculateTaxiFare(1, 8));
         }
 
+        [TestMethod]
+        public void DayTimeFareForMediumDistances()
+        {
+            Assert.AreEqual(168, CalculateTaxiFare(21, 8));
+        }
+
         decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
-            decimal pricePerKm = 5;
+            decimal pricePerKm = distanceInKm > 20? 8: 5;
             return distanceInKm * pricePerKm;
         }
     }
