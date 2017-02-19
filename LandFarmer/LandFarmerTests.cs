1d8f6d6ce63a65d7f722c487a37acf12f5fef24c
rustibi <rustibi@gmail.com>
2017-02-19 12:41:17 +0200

    Calculate Delta

--- LandFarmer/LandFarmerTests.cs
+++ LandFarmer/LandFarmerTests.cs
@@ -4,11 +4,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LandFarmer
 {
     [TestClass]
-    public class UnitTest1
+    public class LandFarmerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DeltaGreaterThenZero()
         {
+            Assert.AreEqual(3132900, CalculateFarmerLand(1, 230, -770000));
+        }
+
+        float CalculateFarmerLand(float a, float b, float c)
+        {
+            
+            a = 1;
+            b = 230;
+            c = -770000;
+            float dt = b * b - 4 * a * c;
+
+            return dt;
+            
+            
+
+            
         }
     }
 }
