550b649927c341b0296835729b5268c029b88930
rustibi <rustibi@gmail.com>
2017-02-19 21:34:10 +0200

    Add Delta Less Then Zero Condition

--- LandFarmer/LandFarmerTests.cs
+++ LandFarmer/LandFarmerTests.cs
@@ -13,17 +13,31 @@ namespace LandFarmer
         }
 
         [TestMethod]
+        public void DeltaLessThenZero()
+        {
+            Assert.AreEqual(0, CalculateSqrtDelta(0, 0, 0));
+        }
+
+        [TestMethod]
         public void CalculateInitialArea()
         {
             Assert.AreEqual(770, CalculateInitialArea(1, 230, -770000));
         }
 
         
+
+
         double CalculateSqrtDelta(float a, float b, float c)
         {
             double dt = b * b - 4 * a * c;
+            if (dt < 0) {
+                return 0;
+            }
             return Math.Sqrt(dt);
         }
+
+
+
 
         double CalculateInitialArea(float a, float b, float c)
         {
