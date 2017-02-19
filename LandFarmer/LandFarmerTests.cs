7a97af5096dad17e42ebf623fcb02b7976e327ec
rustibi <rustibi@gmail.com>
2017-02-19 15:57:57 +0200

    CalculateLandFarmer function renamed as CalculateDelta

--- LandFarmer/LandFarmerTests.cs
+++ LandFarmer/LandFarmerTests.cs
@@ -9,22 +9,19 @@ namespace LandFarmer
         [TestMethod]
         public void DeltaGreaterThenZero()
         {
-            Assert.AreEqual(3132900, CalculateFarmerLand(1, 230, -770000));
+            Assert.AreEqual(3132900, CalculateDelta(1, 230, -770000));
         }
 
-        float CalculateFarmerLand(float a, float b, float c)
+
+       
+        float CalculateDelta(float a, float b, float c)
         {
-            
-            a = 1;
-            b = 230;
-            c = -770000;
             float dt = b * b - 4 * a * c;
-
             return dt;
-            
-            
-
-            
         }
+
+        
+
+        
     }
 }
