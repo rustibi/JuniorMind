aead6b308570efd4d80843a1f04312a5d8c84388
rustibi <rustibi@gmail.com>
2017-02-20 17:52:20 +0200

    Add function for converting to roman for tens

--- ArabicToRomanVer2/ArabicToRomanVer2Tests.cs
+++ ArabicToRomanVer2/ArabicToRomanVer2Tests.cs
@@ -6,18 +6,33 @@ namespace ArabicToRomanVer2
     [TestClass]
     public class ArabicToRomanVer2Tests
     {
+      
+
+        [TestMethod]
+        public void TestTens()
+        {
+            Assert.AreEqual("L", ArabicToRomanVer2(50));
+        }
+
         [TestMethod]
         public void TestOnes()
         {
             Assert.AreEqual("IX", ArabicToRomanVer2(9));
         }
 
+
         string ArabicToRomanVer2 (int number)
         {
-            string[] ones = {"","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-            int[] arabic = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] tens = { " ", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = {" ","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            
+        //  int[] arabic = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             string romanNo = string.Empty;
-            romanNo = ones[arabic[number]];
+
+            romanNo = romanNo + tens[(int)(number / 10)];
+            romanNo = ones[number];
+
+
             return romanNo;
         }
     }
