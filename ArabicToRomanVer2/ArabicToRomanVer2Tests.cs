2831f7ce928ab839a4d51cf8819fa2b6a51c474c
rustibi <rustibi@gmail.com>
2017-02-20 17:11:03 +0200

    Add function for converting to roman for ones unit

--- ArabicToRomanVer2/ArabicToRomanVer2Tests.cs
+++ ArabicToRomanVer2/ArabicToRomanVer2Tests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ArabicToRomanVer2
 {
     [TestClass]
-    public class UnitTest1
+    public class ArabicToRomanVer2Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestOnes()
         {
+            Assert.AreEqual("IX", ArabicToRomanVer2(9));
+        }
+
+        string ArabicToRomanVer2 (int number)
+        {
+            string[] ones = {"","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            int[] arabic = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string romanNo = string.Empty;
+            romanNo = ones[arabic[number]];
+            return romanNo;
         }
     }
 }
