49282a94317728dc555a21595b644ba1a2ec20fe
rustibi <rustibi@gmail.com>
2017-02-20 14:27:39 +0200

    Add  ConvertToForDigitsRomanXCIX test

--- ArabicToRomanConverter/ArabicToRomanConverterTests.cs
+++ ArabicToRomanConverter/ArabicToRomanConverterTests.cs
@@ -37,12 +37,18 @@ namespace ArabicToRomanConverter
             Assert.AreEqual("XLV", ArabicToRomanConverter(45));
         }
 
+        [TestMethod]
+        public void ConvertToForDigitsRomanXCIX()
+        {
+            Assert.AreEqual("XCIX", ArabicToRomanConverter(99));
+        }
+
         string ArabicToRomanConverter(int number)
         {
             string[] romanNo = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM","M" };
             int[] arabicNo = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
             string lst = string.Empty;
-            for (int i = arabicNo.Count() - 1; i > 0; i--)
+            for (int i = arabicNo.Count() - 1; i >= 0; i--)
             {
                 while (number >= arabicNo[i])
                 {
