3a1e03c67157c919bf7d801f0c5b6d3949c8c84a
rustibi <rustibi@gmail.com>
2017-02-24 17:47:15 +0200

    Add CalculatePrefix function

--- Prefix/PrefixTests.cs
+++ Prefix/PrefixTests.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 
 namespace Prefix
 {
     [TestClass]
-    public class UnitTest1
+    public class PrefixTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculatePrefix1()
         {
+            Assert.AreEqual("aa", ComparePrefix("aab", "aac"));
         }
-    }
+
+
+
+      
+        string ComparePrefix(string a, string b)
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == b[i])
+                    builder.Append(a[i]);
+                else
+                    break;
+            }
+            return builder.ToString();
+                        
+         }              
+   }
 }
+    
+
