48124c25248983233408671a95e9ca9acb7eb17b
rustibi <rustibi@gmail.com>
2017-02-24 18:42:26 +0200

    Add Panagram empty project

---
+++ Panagram/PanagramTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class PanagramTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
