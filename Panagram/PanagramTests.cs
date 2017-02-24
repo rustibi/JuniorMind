55e42e7d3c79c2089c9f115e4ff2caf3c0010441
rustibi <rustibi@gmail.com>
2017-02-24 20:05:33 +0200

    Add Calculate Panagram function

--- Panagram/PanagramTests.cs
+++ Panagram/PanagramTests.cs
@@ -7,8 +7,29 @@ namespace Panagram
     public class PanagramTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CountEqualLetters()
         {
+            Assert.AreEqual(26, CalculatePanagram("the quick brown fox jumps over the lazy dog"));
+        }
+
+        int CalculatePanagram(string IsPanagram)
+        {
+            string dictionary = "abcdefghijklmnopqrstuvwxyz";
+            int count = 0;
+      
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                for (int j = 0; j < IsPanagram.Length; j++)
+                {
+                    if (dictionary[i] == IsPanagram[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+
         }
 
 
