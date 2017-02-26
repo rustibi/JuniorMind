a3a59385d344f96be0ef6014727b23d32e983b59
rustibi <rustibi@gmail.com>
2017-02-26 19:11:33 +0200

    Modify CalculateExcelColumnNumber function for all letters

--- ExcelColumnNumbers/ExcelColumnNumbersTests.cs
+++ ExcelColumnNumbers/ExcelColumnNumbersTests.cs
@@ -9,12 +9,51 @@ namespace ExcelColumnNumbers
         [TestMethod]
         public void OneLetterColumn()
         {
-            Assert.AreEqual('b', CalculateExcelNumbers(2));
+            Assert.AreEqual("b", CalculateExcelColumnNumber(2));
         }
 
-        char CalculateExcelNumbers (int columnNo)
-        {   
-            return (char)('a' + columnNo - 1);
+        [TestMethod]
+        public void TwoLettersColumn()
+        {
+            Assert.AreEqual("aa", CalculateExcelColumnNumber(27));
+        }
+
+        [TestMethod]
+        public void TwoLettersColumn2()
+        {
+            Assert.AreEqual("ab", CalculateExcelColumnNumber(28));
+        }
+
+        [TestMethod]
+        public void ThreeLettersColumn()
+        {
+            Assert.AreEqual("aaa", CalculateExcelColumnNumber(53));
+        }
+
+        [TestMethod]
+        public void ThreeLettersColumn2()
+        {
+            Assert.AreEqual("aab", CalculateExcelColumnNumber(54));
+        }
+
+
+
+
+        string CalculateExcelColumnNumber (int columnNo)
+        {
+            int constant = 26;
+            string constantCount = String.Empty;
+            string columnInLetters = String.Empty;
+
+            while (columnNo >= 27)
+            {
+                columnNo = columnNo - constant;
+                constantCount += "a";
+            }
+
+            columnInLetters = constantCount + (char)('a' + columnNo - 1);
+
+            return columnInLetters;
         }
 
     }
