f40afa8f685072c1d6ba97865feb665cc3ba47a7
rustibi <rustibi@gmail.com>
2017-02-28 16:54:32 +0200

    Calculate odds for 6 winning numbers

--- LotoOdds/LotoOddsTests.cs
+++ LotoOdds/LotoOddsTests.cs
@@ -9,9 +9,14 @@ namespace LotoOdds
         [TestMethod]
         public void SixWinningNumbers649()
         {
+            Assert.AreEqual(1/13983816, CalculateLotoOdds(6));
             
         }
 
-        
+        int CalculateLotoOdds(int winningNumbers)
+        {
+            return 1/49 * 2/48 * 3/47 * 4/46 * 5/45 * 6/44;
+        }
+
     }
 }
