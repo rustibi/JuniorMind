1e4eb1320a4f34ed68706e4bad035edd9c6e5849
rustibi <rustibi@gmail.com>
2017-03-01 18:01:46 +0200

    Add Fact and DifFact functions and edit CalculateLotoOdds

---
+++ LotoOdds/BigInteger.cs
@@ -0,0 +1,12 @@
+﻿namespace LotoOdds
+{
+    internal class BigInteger
+    {
+        private int v;
+
+        public BigInteger(int v)
+        {
+            this.v = v;
+        }
+    }
+}
--- LotoOdds/LotoOddsTests.cs
+++ LotoOdds/LotoOddsTests.cs
@@ -1,22 +1,85 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
+
 namespace LotoOdds
 {
     [TestClass]
     public class LotoOddsTests
     {
+         [TestMethod]
+         public void SixWinningNumbers649()
+         {
+             Assert.AreEqual(1/13983816, CalculateLotoOdds(6, 6, 49));
+
+         }
+
+
+         [TestMethod]
+         public void FiveWinningNumbers649()
+         {
+             Assert.AreEqual(1/54201, CalculateLotoOdds(5, 6, 49));
+
+         }
+         
+
         [TestMethod]
-        public void SixWinningNumbers649()
+        public void FactorialFunction()
         {
-            Assert.AreEqual(1/13983816, CalculateLotoOdds(6));
+            Assert.AreEqual(720, Fact(6));
+        }
+
+
+        [TestMethod]
+        public void FactorialDif()
+        {
+            Assert.AreEqual((10068347520), DifFact(49, 6));
+
+        }
+
+
+
+
+
+        long CalculateLotoOdds(int winNo, int catNum, int catDen)
+        {
+            //return (Comb(winNo, catNum)*Comb((catNum - winNo),(catDen - winNo)))/(Comb(catNum, catDen));
+            return (Fact(catNum) * DifFact(catNum, winNo) * DifFact((catDen-catNum), (catNum-winNo)))/(DifFact(catDen, catNum)*Fact(catNum-winNo)*2);
+
+        }
+
+  
+       
+       
+        long DifFact(int number1, int number2)
+        {
+            long result = 1;
+            int total = number1 - number2;
+            if (total == number1)
+                result = 1;
+            while (number1 > total)
+            {
+                result *= number1;
+                number1--;
+            }
             
+            return result;
         }
 
-        int CalculateLotoOdds(int winningNumbers)
+
+        long Fact (int number)
         {
-            return 1/49 * 2/48 * 3/47 * 4/46 * 5/45 * 6/44;
-        }
+        int fact = 1;
+        if (number == 0)
+            fact = 1; 
 
+        while (number > 0) {
+            fact = fact * number;
+            number--; 
+            }
+        return fact;
+
+        }
     }
 }
