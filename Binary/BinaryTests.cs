e01b8e7650f3c424d75c70ea1ca376b24cdc8176
rustibi <rustibi@gmail.com>
2017-03-30 13:02:29 +0300

    Added AND function and test

--- Binary/BinaryTests.cs
+++ Binary/BinaryTests.cs
@@ -39,7 +39,7 @@ namespace Binary
         }
 
         [TestMethod]
-        public void DecimalBinary_NOT49()
+        public void DecimalBinary_NOT_49()
         {
             byte[] binar = { 0, 0, 1, 1, 1, 0 };
             CollectionAssert.AreEqual(binar, NOT(49));
@@ -52,6 +52,41 @@ namespace Binary
             CollectionAssert.AreEqual(binar, OR(5, 3));
         }
 
+        [TestMethod]
+        public void DecimalBinary_AND_5_3()
+        {
+            byte[] binar = { 0, 0, 1 };
+            CollectionAssert.AreEqual(binar, AND(5, 3));
+        }
+
+
+
+        List<byte> AND(int number1, int number2)
+        {
+            List<byte> binarList1 = new List<byte>();
+            List<byte> binarList2 = new List<byte>();
+            List<byte> binarList3 = new List<byte>();
+            binarList1 = DecimalToBinary(number1);
+            binarList2 = DecimalToBinary(number2);
+
+            while (binarList1.Count != binarList2.Count)
+            {
+                if (binarList1.Count < binarList2.Count)
+                    binarList1.Insert(0, 0);
+                else
+                    binarList2.Insert(0, 0);
+            }
+
+            for (int i = 0; i < binarList1.Count; i++)
+            {
+                if (binarList1[i] == 1 && binarList2[i] == 1)
+                    binarList3.Add(1);
+                else
+                    binarList3.Add(0);
+            }
+            return binarList3;
+
+        }
 
         List<byte> OR (int number1, int number2)
         {
