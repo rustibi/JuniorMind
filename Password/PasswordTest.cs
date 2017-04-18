2a6e6131e1200265a0a2923554f38cc3bea81017
rustibi <rustibi@gmail.com>
2017-04-18 14:03:29 +0300

    Added Password function

---
+++ Password/PasswordTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Password
+{
+    [TestClass]
+    public class PasswordTest
+    {
+        [TestMethod]
+        public void GeneratePasswordTest1()
+        {
+            Assert.AreEqual("abc", GeneratePassword(3));
+        }
+
+
+        string GeneratePassword(int x)
+        {
+            string valid = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789!@$?_-";
+            char[] result = new char[x];
+            Random random = new Random();
+            for (int i = 0; i < x; i ++)
+            {
+                result[i] = valid[random.Next(valid.Length)];
+
+            }
+            return new string(result);
+                
+        }
+
+
+
+    }
+}
