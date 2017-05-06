2fbaa9ce232a0eb28c34cf1529c11a00e6d97597
rustibi <rustibi@gmail.com>
2017-05-06 17:12:18 +0300

    Final Fiboncci

---
+++ Fibonacci/FibonacciTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void ShouldReturn5()
+        {      
+            Assert.AreEqual(5, Fibonacci(5));
+        }
+
+        [TestMethod]
+        public void ShouldReturn8()
+        {
+            Assert.AreEqual(8, Fibonacci(6));
+        }
+
+        [TestMethod]
+        public void ShouldReturn1()
+        {
+            Assert.AreEqual(1, Fibonacci(1));
+        }
+
+        [TestMethod]
+        public void ShouldReturn1again()
+        {
+            Assert.AreEqual(1, Fibonacci(2));
+        }
+
+
+        public int Fibonacci (int number)
+        {     
+            if (number < 2)
+                return number;
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+          
+        }
+    }
+}
