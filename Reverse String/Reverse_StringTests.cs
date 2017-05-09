248df8629efee23dd15a423b72634dda771c509d
rustibi <rustibi@gmail.com>
2017-05-09 22:10:33 +0300

    Final ReverseString function with rec

---
+++ Reverse String/Reverse_StringTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reverse_String
+{
+    [TestClass]
+    public class Reverse_StringTests
+    {
+        [TestMethod]
+        public void ShouldReverseString3letters()
+        {
+            Assert.AreEqual("cba", ReverseString("abc"));
+        }
+
+        [TestMethod]
+        public void ShouldReverseString6letters()
+        {
+            Assert.AreEqual("fedcba", ReverseString("abcdef"));
+        }
+
+        string ReverseString (string toReverse)
+        {
+            string reversed = null;
+            if (toReverse.Length == 0)
+                return reversed;
+            return toReverse[toReverse.Length-1] + ReverseString(toReverse.Substring(0, toReverse.Length-1));
+        }
+    }
+}
