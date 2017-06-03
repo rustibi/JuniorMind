d3dc499bacc4b778df0a716c106d92ee727b8fc3
rustibi <rustibi@gmail.com>
2017-06-03 15:50:46 +0300

    Service Final

---
+++ Service/ServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Service
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        [TestMethod]
+        public void ShouldReturnSorted()
+        {
+            Case[] cases = new Case[] { new Case("Volvo", Priority.Low),
+                                        new Case("Saab", Priority.High),
+                                        new Case("Audi", Priority.Medium),
+                                        new Case("Scania", Priority.Medium) };
+            Case[] result = new Case[] { new Case("Saab", Priority.High),
+                                         new Case("Audi", Priority.Medium),
+                                         new Case("Scania", Priority.Medium),
+                                         new Case("Volvo", Priority.Low) };
+            CollectionAssert.AreEqual(result, Sort (cases));
+        }
+
+
+        public enum Priority
+        {
+            Low = 3,
+            Medium = 2,
+            High = 1
+        }
+
+        public struct Case
+        {
+            public string name;
+            public Priority priority;
+            public Case (string name, Priority priority)
+            {
+                this.name = name;
+                this.priority = priority;
+            }
+        }
+
+        public Case[] Sort (Case[] cases)
+        {
+            
+            for (int i = 0; i < cases.Length; i++)
+            {
+                for (int j = 0; j < cases.Length-1; j++)
+                {
+                    if (cases[j].priority > cases[j + 1].priority)
+                    {
+                        var temp = cases[j];
+                        cases[j] = cases[j + 1];
+                        cases[j + 1] = temp;
+                    }
+                }   
+            }
+            return cases;
+        }
+        
+
+
+
+    }
+}
